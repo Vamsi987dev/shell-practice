03fe59df5665bea45d68374ce8e15ca5354767af
vamsi <vamsibattu3015@gmail.com>
2025-05-07 16:28:50 +0530

    commited

--- delete-log.sh
+++ delete-log.sh
@@ -7,3 +7,12 @@ else
     echo "directory doesnot exist"
     exit 1
 fi
+
+files=$(find $source_dir -name "*.log" +14)
+echo "Files: $files"
+
+while IFS= read -r line
+do
+    echo "deleting line: $line"
+    rm -rf $line
+done <<< $files
